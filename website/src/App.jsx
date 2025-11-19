16df04ff93bf4b70a742489d9fdeb9e5cc43be07
ReinaC26 <chensj0314@gmail.com>
2025-11-18 23:05:30 -0500

    Set up project environment and add routing for each pages

---
+++ website/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import ShoppingCart from './pages/ShoppingCart';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
