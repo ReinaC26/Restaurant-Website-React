840f64d780aac7583f98bbd431ea4b6391f3a1a9
ReinaC26 <chensj0314@gmail.com>
2025-11-19 19:41:15 -0500

    Adjust navbar on mobile view

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -7,14 +7,12 @@ function Navbar() {
   const [scrolled, setScrolled] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
-  // Update mobile state on resize
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 768);
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  // Navbar scroll effect
   useEffect(() => {
     const handleScroll = () => setScrolled(window.scrollY > 50);
     window.addEventListener("scroll", handleScroll);
@@ -40,58 +38,60 @@ function Navbar() {
         Between the Bites
       </div>
 
-      {/* Desktop Links */}
-      <ul className="desktop-menu flex flex-row gap-[45px] px-[20px] items-center list-none">
-        <li>
-          <Link
-            to="/"
-            className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
-              isTransparent ? "text-[white]" : "text-[black]"
-            } ${location.pathname === "/" ? "underline underline-offset-4" : ""}`}
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/menu"
-            className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
-              isTransparent ? "text-[white]" : "text-[black]"
-            } ${location.pathname === "/menu" ? "underline underline-offset-4" : ""}`}
-          >
-            Menu
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/about"
-            className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
-              isTransparent ? "text-[white]" : "text-[black]"
-            } ${location.pathname === "/about" ? "underline underline-offset-4" : ""}`}
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/contact"
-            className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
-              isTransparent ? "text-[white]" : "text-[black]"
-            } ${location.pathname === "/contact" ? "underline underline-offset-4" : ""}`}
-          >
-            Contact
-          </Link>
-        </li>
-        <li className="flex items-center">
-          <Link to="/shopping-cart" className="block">
-            <img
-              src={isTransparent ? "/cart1.png" : "/cart2.png"}
-              alt="Cart"
-              className="w-[20px] h-[20px] object-contain"
-            />
-          </Link>
-        </li>
-      </ul>
+      {/* Desktop Menu - only render if not mobile */}
+      {!isMobile && (
+        <ul className="desktop-menu flex flex-row gap-[45px] px-[20px] items-center list-none">
+          <li>
+            <Link
+              to="/"
+              className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
+                isTransparent ? "text-[white]" : "text-[black]"
+              } ${location.pathname === "/" ? "underline underline-offset-4" : ""}`}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/menu"
+              className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
+                isTransparent ? "text-[white]" : "text-[black]"
+              } ${location.pathname === "/menu" ? "underline underline-offset-4" : ""}`}
+            >
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
+                isTransparent ? "text-[white]" : "text-[black]"
+              } ${location.pathname === "/about" ? "underline underline-offset-4" : ""}`}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
+                isTransparent ? "text-[white]" : "text-[black]"
+              } ${location.pathname === "/contact" ? "underline underline-offset-4" : ""}`}
+            >
+              Contact
+            </Link>
+          </li>
+          <li className="flex items-center">
+            <Link to="/shopping-cart" className="block">
+              <img
+                src={isTransparent ? "/cart1.png" : "/cart2.png"}
+                alt="Cart"
+                className="w-[20px] h-[20px] object-contain"
+              />
+            </Link>
+          </li>
+        </ul>
+      )}
 
       {/* Mobile Section: Cart + Hamburger */}
       {isMobile && (
@@ -114,9 +114,9 @@ function Navbar() {
         </div>
       )}
 
-      {/* Mobile Menu - only links, NO cart */}
+      {/* Mobile Menu - only links */}
       {menuOpen && isMobile && (
-        <ul className="mobile-dropdown md:hidden">
+        <ul className="mobile-dropdown">
           <li>
             <Link to="/" onClick={() => setMenuOpen(false)}>
               Home
