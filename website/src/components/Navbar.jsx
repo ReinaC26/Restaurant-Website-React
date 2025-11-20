88c9ecef2f126ce90843bfe9777924d11a64b053
ReinaC26 <chensj0314@gmail.com>
2025-11-19 19:33:13 -0500

    Adjust navbar on mobile view

--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -28,7 +28,8 @@ function Navbar() {
         isTransparent ? "bg-transparent" : "bg-[#fffaf3] shadow-md"
       }`}
     >
-      <div 
+      {/* Logo */}
+      <div
         className={`text-[25px] font-bold transition-colors px-[20px] ${
           isTransparent ? "text-[white]" : "text-[black]"
         }`}
@@ -37,115 +38,75 @@ function Navbar() {
         Between the Bites
       </div>
 
-      {/* Desktop Links */}
-      <ul className="flex flex-row gap-[45px] px-[20px] items-center list-none">
-        <li>
-          <Link
-            to="/"
-            className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
-              isTransparent ? "text-[white]" : "text-[black]"
-            } ${location.pathname === "/" ? "underline underline-offset-4" : ""}`}
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/menu"
-            className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
-              isTransparent ? "text-[white]" : "text-[black]"
-            } ${location.pathname === "/menu" ? "underline underline-offset-4" : ""}`}
-          >
-            Menu
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/about"
-            className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
-              isTransparent ? "text-[white]" : "text-[black]"
-            } ${location.pathname === "/about" ? "underline underline-offset-4" : ""}`}
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/contact"
-            className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
-              isTransparent ? "text-[white]" : "text-[black]"
-            } ${location.pathname === "/contact" ? "underline underline-offset-4" : ""}`}
-          >
-            Contact
-          </Link>
-        </li>
-        <li className="flex items-center">
-          <Link to="/shopping-cart" className="block">
-            <img 
-              src={isTransparent ? "/cart1.png" : "/cart2.png"} 
-              alt="Cart" 
-              className="w-[20px] h-[20px] object-contain" 
-            />
-          </Link>
-        </li>
-      </ul>
-
-      {/* Hamburger for Mobile */}
-      {isMobile && (
-        <button
-          className={`text-3xl focus:outline-none transition-colors ${
-            isTransparent ? "text-white" : "text-black"
-          }`}
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          ☰
-        </button>
+      {/* Desktop Menu */}
+      {!isMobile && (
+        <ul className="flex flex-row gap-[45px] px-[20px] items-center list-none">
+          {["/", "/menu", "/about", "/contact"].map((path, idx) => {
+            const name = ["Home", "Menu", "About", "Contact"][idx];
+            return (
+              <li key={path}>
+                <Link
+                  to={path}
+                  className={`no-underline hover:underline hover:underline-offset-4 transition-colors ${
+                    isTransparent ? "text-[white]" : "text-[black]"
+                  } ${location.pathname === path ? "underline underline-offset-4" : ""}`}
+                >
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
+          <li className="flex items-center">
+            <Link to="/shopping-cart" className="block">
+              <img
+                src={isTransparent ? "/cart1.png" : "/cart2.png"}
+                alt="Cart"
+                className="w-[20px] h-[20px] object-contain"
+              />
+            </Link>
+          </li>
+        </ul>
       )}
 
-      {/* Mobile Menu */}
-      {menuOpen && (
-        <ul className="absolute top-16 right-0 w-48 bg-[#fffaf3] flex flex-col gap-4 p-4 rounded-b-lg shadow-lg text-black">
-          <li>
-            <Link 
-              to="/" 
-              onClick={() => setMenuOpen(false)}
-              className={`hover:underline ${location.pathname === "/" ? "underline" : ""}`}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link 
-              to="/menu" 
-              onClick={() => setMenuOpen(false)}
-              className={`hover:underline ${location.pathname === "/menu" ? "underline" : ""}`}
-            >
-              Menu
-            </Link>
-          </li>
-          <li>
-            <Link 
-              to="/about" 
-              onClick={() => setMenuOpen(false)}
-              className={`hover:underline ${location.pathname === "/about" ? "underline" : ""}`}
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link 
-              to="/contact" 
-              onClick={() => setMenuOpen(false)}
-              className={`hover:underline ${location.pathname === "/contact" ? "underline" : ""}`}
-            >
-              Contact
-            </Link>
-          </li>
-          <li>
-            <Link to="/shopping-cart">
-              <img src="/cart2.png" alt="Cart" className="w-4 h-4" />
-            </Link>
-          </li>
+      {/* Mobile: Hamburger + Cart */}
+      {isMobile && (
+        <div className="flex items-center gap-4">
+          <Link to="/shopping-cart" className="block z-50">
+            <img
+              src={isTransparent ? "/cart1.png" : "/cart2.png"}
+              alt="Cart"
+              className="w-[20px] h-[20px] object-contain"
+            />
+          </Link>
+          <button
+            className={`text-3xl focus:outline-none z-50 ${
+              isTransparent ? "text-white" : "text-black"
+            }`}
+            onClick={() => setMenuOpen(!menuOpen)}
+            aria-label="Toggle menu"
+          >
+            {menuOpen ? "✕" : "☰"}
+          </button>
+        </div>
+      )}
+
+      {/* Mobile Dropdown */}
+      {isMobile && menuOpen && (
+        <ul className="absolute top-16 right-0 w-48 bg-[#fffaf3] flex flex-col gap-4 p-4 rounded-b-lg shadow-lg text-black z-40">
+          {["/", "/menu", "/about", "/contact"].map((path, idx) => {
+            const name = ["Home", "Menu", "About", "Contact"][idx];
+            return (
+              <li key={path}>
+                <Link
+                  to={path}
+                  onClick={() => setMenuOpen(false)}
+                  className={`hover:underline ${location.pathname === path ? "underline" : ""}`}
+                >
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       )}
     </nav>
