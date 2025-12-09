8ecd515ba3eed1bc348e49cb49ce80e2196c50f4
ReinaC26 <chensj0314@gmail.com>
2025-12-08 23:51:44 -0500

    update deployed frontend link

--- website/backend/server.js
+++ website/backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(cors({
     origin: [
-      'https://restaurant-website-frontend-tilq.onrender.com',
+      'https://between-the-bites.onrender.com',
       'http://localhost:5173'  // for local development
     ],
     credentials: true
